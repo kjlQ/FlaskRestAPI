3c07530111deef8d510fd3f3120c2ae4a3e3e129
Roman <rett8716@gmail.com>
2024-04-18 22:40:55 +0300

    get,post integration

--- app.py
+++ app.py
@@ -1,3 +1,39 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+stores = [
+    {
+        "name": "My store",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+@app.route("/store", methods=["GET"])
+def get_stores():
+    return jsonify({"stores": stores})
+
+@app.route("/store", methods=["POST"])
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return jsonify(stores), 201
+
+@app.route("/store/add/item", methods=["POST"])
+def add_item():
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == request_data["store_name"]:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return jsonify(stores), 201
+    return {"message":"Not found"}, 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
