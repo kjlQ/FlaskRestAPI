591e8d98fd5f8ef1c777b02235d453ac6d65bfbd
Roman <rett8716@gmail.com>
2024-06-04 23:04:44 +0300

    DB improvements

--- app.py
+++ app.py
@@ -1,39 +1,44 @@
-from flask import Flask, request, jsonify
+import uuid
+from flask import Flask, jsonify, request
+from db import items, stores
 
 app = Flask(__name__)
 
-stores = [
-    {
-        "name": "My store",
-        "items": [
-            {
-                "name": "Chair",
-                "price": 15.99
-            }
-        ]
-    }
-]
-
-@app.route("/store", methods=["GET"])
+@app.get("/store")
 def get_stores():
-    return jsonify({"stores": stores})
+    return jsonify({"stores": list(stores.values())})
 
-@app.route("/store", methods=["POST"])
+
+@app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
-    return jsonify(stores), 201
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
 
-@app.route("/store/add/item", methods=["POST"])
-def add_item():
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == request_data["store_name"]:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return jsonify(stores), 201
-    return {"message":"Not found"}, 404
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "Store not found"}, 404
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+    return item, 201
+
+@app.get("/items")
+def get_all_items():
+    return {"items": list(items.values())}
+
+@app.get("/item")
+def get_item():
+    item_data = request.get_json()
+    uuid = item_data["uuid"]
+    if uuid in items:
+        return items[uuid]
+    else:
+        return {"message": "Item not found"}, 404
 
 if __name__ == "__main__":
     app.run(debug=True)
