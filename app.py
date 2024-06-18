75917394eca419de4a595500f1301bbcc67060e7
Roman <rett8716@gmail.com>
2024-06-18 17:22:30 +0300

    Delete/update endpoint for item

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, jsonify, request
+from flask_smorest import abort
 from db import items, stores
 
 app = Flask(__name__)
@@ -21,11 +22,30 @@ def create_store():
 def create_item():
     item_data = request.get_json()
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
     items[item_id] = item
     return item, 201
+
+@app.delete("/item")
+def delete_item():
+    item_data = request.get_json()
+    try:
+        del items[item_data["uuid"]]
+    except KeyError:
+         return {"message":"Item not found"}, 404
+    return {"message":"Item deleted"}, 200
+
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    try:
+        items[item_id].update(item_data)
+    except KeyError:
+         return {"message":"Item not found"}, 404
+    return {"message":"Item Updated"}, 200
 
 @app.get("/items")
 def get_all_items():
@@ -38,7 +58,7 @@ def get_item():
     if uuid in items:
         return items[uuid]
     else:
-        return {"message": "Item not found"}, 404
+        abort(404, message="Item not found")
 
 if __name__ == "__main__":
     app.run(debug=True)
